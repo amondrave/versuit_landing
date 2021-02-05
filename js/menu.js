6fcd0865fb77e85500d021b4b90b1ea065e247ac
angel mondragon <amondrave54@gmail.com>
2021-02-04 22:01:10 -0500

    menu hamburguesa hecho

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,17 @@
-jQuery('document').ready(function ($) {
+$('document').ready(function () {
     var menuBtn = $('.menu-icon');
     var menu = $('.navigation ul');
-
+    var algo = $('.logotipo')
     menuBtn.click(function () {
         if (menu.hasClass('show')) {
             menu.removeClass('show')
         } else {
             menu.addClass('show')
+            console.log("Sirve")
         }
+
     });
     console.log(menuBtn);
     console.log(menu);
+    console.log("HI")
 });
